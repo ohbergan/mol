dae0e2b515dd81a8e14036b8d5096bc7d1200d05
Aliabbas2003 <ali2606@skole.vtfk.no>
2022-03-31 12:19:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,18 @@
 
 
 # Oppgave 5 & 6 - Ali
-import pyodbc
+import Pyodbc
 
-current_path = pyodbc.path.dirname(__file__)
+current_path = Pyodbc.path.dirname(__file__)
 
 conn_str = (r'DRIVER={microsoft access driver (*.mdb, *.accdb)};'
 r'dbq=' + current_path + '\medlemsregistrer.accdb;')
 
-conn = pyodbc.connect(conn_str)
+conn = Pyodbc.connect(conn_str)
 cursor = conn.cursor()
 sql_str = "select * from kontaktinfo_python"
 cursor.execute(sql_str)
 for row in cursor.fetchall():
     print(row)
+
+    
