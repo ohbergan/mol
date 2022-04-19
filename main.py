08d477678467e596249c016559b1435e720c8ea6
Aliabbas2003 <102141061+Aliabbas2003@users.noreply.github.com>
2022-04-19 09:09:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,19 @@
 from turtle import clear
 import pyodbc
 import os
+import csv
+
+with open('employee_birthday.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
 
 current_path = os.path.dirname(__file__)
 
@@ -22,7 +35,7 @@ def close():
 
 def menu5():
     clear()
-    print("betalte medlemmer.")
+    print("vedlikehold.")
     ans=input("\velg tall.")
 
     if ans == "1":
