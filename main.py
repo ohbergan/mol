2abe12938ede7e4c6da21c26d3e5bf44c1c44b29
Aliabbas2003 <102141061+Aliabbas2003@users.noreply.github.com>
2022-04-19 09:28:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,7 @@
 from turtle import clear
 import pyodbc
 import os
-import csv
 
-with open('medlemsliste.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} {row[1]}, {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
 
 current_path = os.path.dirname(__file__)
 
