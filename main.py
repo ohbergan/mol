9b8670a2c2fece4ef3bebe9e6c9d932b09f8980e
Aliabbas2003 <102141061+Aliabbas2003@users.noreply.github.com>
2022-05-05 13:23:17 +0200

    g

--- main.py
+++ main.py
@@ -1,93 +1,103 @@
-# Oppgave 1, 2 & 3 - Ole Halvor
-
-
-# Oppgave 4 - Emil
-
-
-# Oppgave 5 & 6 - Ali
-
+import csv
 import pyodbc
 import os
+from datetime import datetime
 
-
-
-
-
+# Saves the path for the running script so that it can be used to reference files
 current_path = os.path.dirname(__file__)
 
-conn_str = (r'DRIVER={microsoft access driver (*.mdb, *.accdb)};'
-            r'dbq=' + current_path + '\medlemsregister.accdb;')
+# The connection string tells PYODBC to connect to the database
+conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=' + current_path + '\Medlemsregister.accdb;')
+
+# Connect to the database
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
-    
 
-  
-
+# A function used to clear the screen
 
 
-
-def clear():
-     command = 'clear'
-     if os.name in ('nt', 'dos'): 
-         command = 'cls'
-     os.system(command)
-
+def clearScreen():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
 
 
 def menu5():
-    print("\nvedlikehold\n")
-    print("1. \nLegg til en medlemstype (Fullt medlem, støttemedlem osv)")
-    print("2. \nSlett en medlemstype")
-    print("3. \nØk kontingentenmed 10%")
-    print("4. \nSkrive medlemslisten ut til en csv-fil")
-    print("5. \ntilbake til hovedmeny")
-    ans = input("\nhva vil du velge i dag?\n")
-
-    if ans == "1":
-        print("Legg til en medlemstype (Fullt medlem, støttemedlem osv).")
-        MTypeNavn = int(input("\nligg inn MTypenavn: \n"))
-        Kontigent = int(input("\nligg inn kotigent: \n"))
-        sql_str=f"INSERT INTO Medlemstyper (MTypeNavn, Kontigent) VALUES ('{MTypeNavn}','{Kontigent}');"
-        cursor.execute(sql_str)
-        input()
+            clearScreen()
+            print("\nHovedmeny for endring av medlem\n")
+            print("1. Legg til en medlemstype (Fullt medlem, støttemedlem osv)")
+            print("2. Slett en medlemstype")
+            print("3. Øk kontingentenmed 10%")
+            print("4. Skrive medlemslisten ut til en csv-fil")
+            print("5. Tilbake til Hovedmenyen")
+            ans = input("\nHva ønsker du å gjøre? Velg tall.\n")
 
 
-    elif ans == "2":
-        print("Slett en medlemstype?")
-        MTypeID = int(input("\nMTypeID skal bli slettes: \n"))
-        sql_str=f"DELETE FROM MedlemsType WHERE MTypeID like {MTypeID}"
-        cursor.execute(sql_str)
+            if ans == "1":
 
-    
-    elif ans == "3":
-        print("hvor mye vil du ligge i kortinget?")
-        sql_str=f"select {input}*(1+10/100)"
-        cursor.execute(sql_str)
+                Medlemstype_navn = input("Navn på medlemstype: ")
+                Kontigent = int(input("Pris på kontigent: "))
 
 
-    elif ans == "4":
-        print("Skrive medlemslisten ut til en csv-fil.")
-        sql_str="SELECT Medlemmer.[Fornavn] & " " & [Etternavn] AS Navn, Medlemmer.Telefon, Medlemmer.[E-post], Medlemmer.FødselsdatoFROM Poststeder INNER JOIN (Medlemstyper INNER JOIN Medlemmer ON Medlemstyper.MTypeID = Medlemmer.MTypeID) ON Poststeder.Postnr = Medlemmer.Postnr;"
-        cursor.execute(sql_str)
+                cursor.execute(
+                    f"INSERT INTO Medlemstyper (MTypeNavn, Kontigent) VALUES ('{Medlemstype_navn}', {Kontigent});")
+                conn.commit()
 
-        
-           
+            elif ans == "2":
 
-     
-    
+                print("Her kan du slette en medlemstype")   
+                Medlemstype_ID = int(input("\nMedlemstypeID til medlemstype som skal slettes: \n"))
+                
+                sql_str = f"DELETE FROM Medlemstyper WHERE MTypeID like {Medlemstype_ID}"
+                cursor.execute(sql_str)
+                conn.commit()
+
+                print("Medlemstypen ble slettet!") 
+                input("Trykk ENTER for å gå videre: ")
+
+            elif ans == "3":
+                mTypeId = int(input("\nOppgi MTypeID: \n"))
+                sql_str =  f"UPDATE Medlemstyper SET Kontigent = Kontigent*1.1 WHERE MTypeID = {mTypeId}"
+                cursor.execute(sql_str)
+                conn.commit()
+
+                print("Kontigenten er oppdatert med 10%!") 
+                input("Trykk ENTER for å gå videre: ")
+
+            elif ans =="4":
+                 # Run an SQL qurey that selects a list of members
+
+                sql_str = "select MedlemsID, Fornavn, Etternavn, Mobil from Medlemmer"
+
+                cursor.execute(sql_str)
+
+                # Store header and data info for the CSV
+                header = ['memberid', 'first_name', 'last_name', 'mobile']
+                data = []
+
+                # Clear the screen
+                clearScreen()
+
+                # Add all table rows to data list
+                for row in cursor.fetchall():
+                    data.append([row[0], row[1] or "", row[2] or "", row[3] or ""])
 
 
+                # Create CSV file
+                with open('exports/members.csv', 'w', encoding='UTF8', newline='') as f:
 
-menu5()
+                    # Create a writer
+                    writer = csv.writer(f)
 
-def menu6():
-    clear()
+                    # Write header
+                    writer.writerow(header)
+
+                    # Write multiple rows
+                    writer.writerows(data)
 
 
-
-    
-
-
-
-    
+                print("CSV fil opprettet i exports mappen")
+                input("Trykk ENTER for å gå videre: ")
